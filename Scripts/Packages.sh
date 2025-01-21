148e6ac231b60f050b41fe70752edf6e7f88d885
VIKING <viking.yfy@qq.com>
2025-01-21 22:09:56 +0800

    项目调整

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -40,10 +40,12 @@ UPDATE_PACKAGE() {
 
 	# 处理克隆的仓库
 	if [[ $PKG_SPECIAL == "pkg" ]]; then
-		find ./$REPO_NAME/*/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -prune -exec cp -rf {} ./ \;
-		rm -rf ./$REPO_NAME/
+		for KEYWORD in "${KEYWORDS[@]}"; do
+			find ./$REPO_NAME/*/ -maxdepth 3 -type d -iname "*$KEYWORD*" -prune -exec cp -rf {} ./ \;
+		done
+		rm -rf ./$REPO_NAME
 	elif [[ $PKG_SPECIAL == "name" ]]; then
-		mv -f $REPO_NAME $PKG_NAME
+		mv -f $REPO_NAME ${KEYWORDS[0]}
 	fi
 }
 
